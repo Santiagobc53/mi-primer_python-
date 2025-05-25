b50b5a84a3fee7cbfc50f4ada7b0b6a45914a2e7
Fabián Santiago Barrera Caro <santiagobc53@gmail.com>
2025-05-25 18:21:55 -0500

    Corrijo import typo y cambio y a lista en grafico_lineal.py

--- practica_graficas.py
+++ practica_graficas.py
@@ -1,10 +1,9 @@
-import matplotlib.pylot as plt 
+import matplotlib.pyplot as plt   # corregido de 'pylot' a 'pyplot'
 
-x = [1,2,3,4,5]
-y = {2,4,6,8,10}
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]              # uso de lista en lugar de set
 
-plt.plot(x,y) 
-
+plt.plot(x, y)
 plt.title("Gráfico de línea Simple")
 plt.xlabel("Eje X")
 plt.ylabel("Eje Y")
